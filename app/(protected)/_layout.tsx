2a291cc5a61bb6fc485c8c29037f2b01bcec09a4
Dhananjaya Swarnahansa <143073814+dananjayaswarnahansa17@users.noreply.github.com>
2026-07-15 14:39:40 +0530

    Refactor project structure: update asset paths, add MeterReadingDashboard component, and enhance dashboard layout

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,26 +1,21 @@
 import { Redirect, Stack } from "expo-router";
 import { useAuth } from "../../src/context/AuthContext";
 
+export default function ProtectedLayout() {
+  const { session, loading } = useAuth();
 
-export default function ProtectedLayout(){
-
-  const {session,loading} = useAuth();
-
-
-  if(loading){
+  if (loading) {
     return null;
   }
 
-
-  if(!session){
+  if (!session) {
     return <Redirect href="/" />;
   }
-
 
   return (
     <Stack
       screenOptions={{
-        headerShown:true
+        headerShown: true,
       }}
     />
   );
